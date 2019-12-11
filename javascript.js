7fdbd6db7cf1cccc8344c896463a29d2049d909c
Austin T <austin.taranto@gmail.com>
2019-12-11 17:30:43 -0500

    fixes to 3rd trick

--- javascript.js
+++ javascript.js
@@ -5,3 +5,31 @@ const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
       p.style.color = '#BADA55';
       p.style.fontSize = '50px';
     }
+
+    // Regular
+    console.log("hello");
+
+    // Interpolated
+    console.log("Hello I am %s.", "Bob")
+    // console.log(`Hello I am ${var}`)
+
+    // Styled
+    console.log("%c I am some great text", "font-size:25px; background: blue; text-shadow: 10px 10px 0 red")
+
+    // warning!
+
+    // Error :|
+
+    // Info
+
+    // Testing
+
+    // clearing
+
+    // Viewing DOM Elements
+
+    // Grouping together
+
+    // counting
+
+    // timing
